54043250269840689e348fa86a9e673ad979df25
Markus Waltre <markus.waltre@gmail.com>
2014-08-18 21:04:26 +0200

    problem 13

---
+++ problems/12.js
@@ -0,0 +1,21 @@
+
+var sum = 0;
+var number = 1;
+while(sum+=number) {
+	number+=1;
+
+	if(divisors(sum) > 500) {
+		console.log(sum);
+		break;
+	}
+}
+
+function divisors (number) {
+	var div = 0;
+	
+	for(var i=0; i<=number; i++) {
+		if(number%i === 0) div++;
+	}
+	console.log(div);
+	return div;
+}
